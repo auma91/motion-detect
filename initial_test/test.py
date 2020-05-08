7d7967dfb944924d14eda22a9ac5817ec5284385
Aurangzeb Malik <auma9171@colorado.edu>
2020-05-08 15:00:00 -0600

    improving raspbian code

--- initial_test/test.py
+++ initial_test/test.py
@@ -3,14 +3,31 @@ import time, urllib.request, json
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.OUT)  # Define pin 8 as an output pin
-while True:
-	with urllib.request.urlopen("https://aurangzeblight.herokuapp.com/summary?psw=Aurangzebiscool123@") as url:
+base_link = "https://aurangzeblight.herokuapp.com/"
+endpoint = "summary"
+secret_query = "?psw=Aurangzebiscool123@"
+def get_JSON(link):
+	with urllib.request.urlopen() as url:
 		data = json.loads(url.read().decode())
-		print(data)
-	if data['On']:
-		GPIO.output(8, 1)  # Outputs digital HIGH signal (5V) on pin 8
-		time.sleep(5)  # Time delay of 1 second
-	else:
-		GPIO.output(8, 0)  # Outputs digital LOW signal (0V) on pin 8
-		time.sleep(1)  # Time delay of 1 second
-
+		return data
+def adjust_LED():
+	fail_count = 0
+	while True:
+		data = None
+		try:
+			data = get_JSON(base_link+endpoint+secret_query)
+			fail_count = 0
+		except:
+			fail_count+=1
+			print("Error Reaching Endpoint, trying again ...")
+			if fail_count == 10:
+				print("10 exceptions raised, check internet connection\nShutting program down.")
+				break
+			else:
+				continue
+		if data['On']:
+			GPIO.output(8, 1)  # Outputs digital HIGH signal (5V) on pin 8
+			time.sleep(5)  # Time delay of 1 second
+		else:
+			GPIO.output(8, 0)  # Outputs digital LOW signal (0V) on pin 8
+			time.sleep(1)  # Time delay of 1 second
